7fb9b7cd4c9ea68536a38d0a2cc8386a4e0aa99d
rhea <rheabutala597@gmail.com>
2019-07-30 15:26:49 +0530

    login with db connect

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express=require('express');
 const app = express();
 const Joi = require('joi');
 var mysql= require('mysql');
+var bodyParser = require('body-parser');
 //var routes = require('./route.js');
 var connection= require('./connect.js');
 var session = require('express-session');
+var path = require('path');
 
 app.use(express.json());
 app.use(session({
@@ -14,7 +16,63 @@ app.use(session({
 }));
 
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+     //response.render('./index.html');
+});
+
+app.get('/registration.html', function(request, response) {
+	response.sendFile(path.join(__dirname + '/registration.html'));
+     //response.render('./index.html');
+});
+
+
+app.post('/auth', function(request, response) {
+	var user_handle = request.body.user_handle;
+	var password = request.body.password;
+	if (user_handle && password) {
+		connection.query('SELECT * FROM user WHERE user_handle = ? AND password = ?', [user_handle, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.user_handle = user_handle;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+
+app.post('/auth2',function(request,response){
+	var user_handle = request.body.user_handle;
+	var password = request.body.password;
+	var Name = request.body.Name;
+	var email = request.body.email;
+	connection.query('insert into user (Name, email,password,user_handle) values()')
+})
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.user_handle + '!');
+        //response.redirect('login.html');
+		//console.log("hii");
+          //response.render('index.html');
+		//response.sendFile(path.join(__dirname + '/login.html'));
+        
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`listening on port ${port}`));
